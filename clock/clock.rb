e9ca1f08514b9f881e680735648e638350a9bd56
Jeremy Wilkins <jeremy@jdwilkins.co.uk>
2015-06-23 20:15:57 +0100

    Updated clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 
 set :bind, '0.0.0.0'
+#set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/' do
-  "Time is #{Time.now}"
+  @time = Time.now
+  @hour = @time.hour
+  @minute = @time.min
+  @second = @time.sec
+  erb :index
 end
